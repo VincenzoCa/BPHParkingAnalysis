825a8797958419ae56e1fcde3c5d50057d42ca5d
Vin Ca <vinca91@live.it>
2019-10-29 02:52:14 +0000

    config to launch jobs

---
+++ scripts/config_runAnalyis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                                                                                                                 
+cd ../../../RDFanalysis
+source settings.sh
+python analyzer_RDF.py --isEleCh 1 --inList DUMMYINPUTFILELIST --JOBid DUMMYJOBID --outFile DUMMYOUTFILENAME
