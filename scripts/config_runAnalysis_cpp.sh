228adfa806062da4b95f641ae7fc246418a6129b
Vin Ca <vinca91@live.it>
2019-11-15 01:12:20 +0000

    script config for analyzer in cpp

---
+++ scripts/config_runAnalysis_cpp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                                                                                                                 
+cd ../../../RDFanalysis
+source settings.sh
+./analyzer_RDF --inList DUMMYINPUTFILELIST --JOBid DUMMYJOBID --outFile DUMMYOUTFILENAME
