b40588faef987dc44457782ee72cf9c0f599baa3
Vin Ca <vinca91@live.it>
2019-11-15 00:33:25 +0000

    launch jobs with histoProducer_RDF.py

---
+++ scripts/config_histoProducer_RDF.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash                                                                                                                                                                 
+cd ../../../macro
+source settings.sh
+python histoProducer_RDF.py --inList DUMMYINPUTFILELIST --JOBid DUMMYJOBID --outFile DUMMYOUTFILENAME
